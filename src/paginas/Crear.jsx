dcc89d322ce89f4be11183682b3f6acfd0748e8b
Shiao-Li <roberto.shiao@epn.edu.ec>
2023-08-28 14:36:02 -0500

    correccion vista crear

--- src/paginas/Crear.jsx
+++ src/paginas/Crear.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Formulario } from '../componets/Formulario'
-import Tabla from '../componets/Tabla'
+
 
 
 const Crear = () => {
@@ -9,7 +9,7 @@ const Crear = () => {
             <h1 className='font-black text-4xl text-gray-500'>Pacientes</h1>
             <hr className='my-4' />
             <p className='mb-8'>Este módulo te permite registrar un nuevo paciente</p>
-            <Tabla/>
+            <Formulario/>
         </div>
       )
 }
